3b9ea33828999fcb939d59fa84a995ec01a98adc
hasi <hasinet@163.com>
2020-01-07 16:42:59 +0800

    加入 parcel 将 button 组件改为单文件组件

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="h-button">hasi</button>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+.h-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
